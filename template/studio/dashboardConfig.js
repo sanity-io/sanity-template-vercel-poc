44710fc57ab218dc4cedeb97481bebc4ac9c3187
Bjørge Næss <bjoerge@gmail.com>
2019-11-18 14:21:58 +0100

    Initial

---
+++ template/studio/dashboardConfig.js
@@ -0,0 +1,14 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials'
+    },
+    {name: 'structure-menu'},
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recently edited', order: '_updatedAt desc', limit: 10, types: []},
+      layout: {width: 'medium'}
+    }
+  ]
+}
