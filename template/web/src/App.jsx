f592cdd79011ae20d2dbb0ec3da13ffc26e4ebb3
Bjørge Næss <bjoerge@gmail.com>
2020-06-03 16:14:05 +0200

    Replace preview image

--- template/web/src/App.jsx
+++ template/web/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div style={{ margin: '40px' }}>
+      <h1>👋 Hello World</h1>
     </div>
   )
 }
